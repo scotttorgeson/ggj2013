40fd8f40539122eebb4b2ef90f8a89f476653f71
Andrew Jones <andrew.b.jones@gmail.com>
2013-01-26 15:54:01 -0700

    Renamed unit prefabs

--- HeartGame/Assets/Scripts/UnitUpgrade.cs
+++ HeartGame/Assets/Scripts/UnitUpgrade.cs
@@ -5,6 +5,7 @@ public class UnitUpgrade : MonoBehaviour {
 	public GameObject spawnUnit;
 	public UnitUpgrade[] upgrades;
 	public int cost;
+	public int spawnNumber=2;
 	
 	public GUIStyle upgradeButtonStyle;
 	public GUIContent upgradeButtonContent;
