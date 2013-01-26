e71b67ccf868642842c8c5d29d031f19bd5b0c1f
Andrew Jones <andrew.b.jones@gmail.com>
2013-01-25 23:55:12 -0700

    Few changes

--- HeartGame/Assets/SkipSpawnScript.cs
+++ HeartGame/Assets/SkipSpawnScript.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using System.Collections;
 
 public class SkipSpawnScript : MonoBehaviour {
-	public GameObject sourcePlayer;
-	public GameObject targetPlayer;
+	public PlayerScript sourcePlayer;
+	public PlayerScript targetPlayer;
 	public int moneyCost;
 	
 	// Use this for initialization
 	void Start () {		
-		var gameLoop = (GameLoop)GameObject.FindObjectOfType(typeof(GameLoop));
+		var gameLoop = (GameLoop)(GameObject.FindObjectOfType(typeof(GameLoop)));
 		gameLoop.skipEnemySpawn = true;
 		sourcePlayer.currentMoney -= moneyCost;
 		GameObject.Destroy(gameObject);
