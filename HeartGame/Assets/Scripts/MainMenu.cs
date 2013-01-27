1b32834570ea5fbb8caa2d09dfa4547a9a0550bc
Andrew Jones <andrew.b.jones@gmail.com>
2013-01-27 05:24:52 -0700

    credits on main menu

--- HeartGame/Assets/Scripts/MainMenu.cs
+++ HeartGame/Assets/Scripts/MainMenu.cs
@@ -15,5 +15,48 @@ public class MainMenu : MonoBehaviour {
 		if(GUI.Button (new Rect(20,40,80,20), "Start Game")){
 			Application.LoadLevel(1);
 		}
+		
+		var RightAlign = new GUIStyle(){
+			alignment = TextAnchor.MiddleRight,
+		};
+		
+		var LeftAlign = new GUIStyle(){
+			alignment = TextAnchor.MiddleLeft
+		};
+		LeftAlign.normal.textColor = Color.white;
+		LeftAlign.active.textColor = Color.white;
+		
+		GUILayout.BeginArea(new Rect(Screen.width - 220, 10, 200, Screen.height - 20));
+		GUILayout.BeginVertical();
+		
+		GUILayout.Label("CREDITS");
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Scott Torgesen", LeftAlign);
+		GUILayout.Label("Programmer", RightAlign);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Gil Bojorquez", LeftAlign);
+		GUILayout.Label ("Artist", RightAlign);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Kyle Chittenden", LeftAlign);
+		GUILayout.Label ("Artist", RightAlign);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Andrew Jones", LeftAlign);
+		GUILayout.Label ("Programmer", RightAlign);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Eric Rios", LeftAlign);
+		GUILayout.Label ("Artist", RightAlign);
+		GUILayout.EndHorizontal();
+		
+		GUILayout.EndVertical();
+		GUILayout.EndArea();
 	}
 }
