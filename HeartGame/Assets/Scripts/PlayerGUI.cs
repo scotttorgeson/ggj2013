e1a07855fb3995abbfa793e7d14ffb4a03c3c65f
Andrew Jones <andrew.b.jones@gmail.com>
2013-01-26 20:00:48 -0700

    Added minimap

--- HeartGame/Assets/Scripts/PlayerGUI.cs
+++ HeartGame/Assets/Scripts/PlayerGUI.cs
@@ -1,105 +1,106 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerGUI : MonoBehaviour {
+public class PlayerGUI : MonoBehaviour
+{
 	
 	private GameObject selectedSpawner;
 	public GameObject selectionRing;
-	
 	public GUIStyle upgradeButtonStyle;
 	public GUIContent upgradeButtonContent;
 	public Rect upgradeButtonRect;
-	
 	public GUIStyle resourceTextStyle;
 	public Rect resourceTextRect;
-	
 	public GUIStyle mapStyle;
-	public Vector2 mapSize = new Vector2(128, 512);
-	public Rect sceneSize = new Rect(-250, 225, 400, 1575);
+	public Vector2 mapSize = new Vector2 (128, 512);
+	public Rect sceneSize = new Rect (-250, 225, 400, 1575);
 	public float iconSize = 3;
-	public GUIStyle enemyStyle;
-	public GUIStyle friendlyStyle;
+	public Color enemyColor;
+	public Color friendlyColor;
+	public Texture iconTexture;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		UpdateSelection();
+		UpdateSelection ();
 	}
 	
-	Vector2 getMapPos(Rect mapRect, Vector3 worldPos){
-		return new Vector2((worldPos.x - sceneSize.x)/sceneSize.width,
-			mapRect.height - (worldPos.z - sceneSize.y)/sceneSize.height);
-	}
-	
-	void OnGUI()
+	Vector2 getMapPos (Rect mapRect, Vector3 worldPos)
 	{
-		var mapRect = new Rect(Screen.width - mapSize.x, Screen.height - mapSize.y, mapSize.x, mapSize.y);
-		GUI.BeginGroup(mapRect, mapStyle);
-		var icons = GameObject.FindGameObjectsWithTag("EnemyUnit");
-		foreach(var icon in icons){
-			var mapPos = getMapPos(mapRect, icon.transform.position);
-			GUI.Box (new Rect(mapRect.x + mapPos.x, mapRect.y + mapPos.y, iconSize, iconSize), "", enemyStyle);
-		}		
-				
-		icons = GameObject.FindGameObjectsWithTag("EnemyUnit");
-		foreach(var icon in icons){
-			var mapPos = getMapPos(mapRect, icon.transform.position);
-			GUI.Box (new Rect(mapRect.x + mapPos.x, mapRect.y + mapPos.y, iconSize, iconSize), "", friendlyStyle);
+		return new Vector2 ((worldPos.x - sceneSize.x) * mapRect.width / sceneSize.width,
+			mapRect.height - (worldPos.z - sceneSize.y) * mapRect.height / sceneSize.height);
+	}
+	
+	void OnGUI ()
+	{
+		var mapRect = new Rect (Screen.width - mapSize.x - 10, Screen.height - mapSize.y - 10, mapSize.x, mapSize.y);
+		GUI.BeginGroup (mapRect, mapStyle);
+		var icons = GameObject.FindGameObjectsWithTag ("EnemyUnit");
+		GUI.color = Color.red;
+		foreach (var icon in icons) {
+			var mapPos = getMapPos (mapRect, icon.transform.position);
+			var unitMove = icon.GetComponent<UnitMovement>();
+			if(unitMove!=null && unitMove.IsVisible()){
+				GUI.DrawTexture (new Rect (mapPos.x, mapPos.y, iconSize, iconSize),
+					iconTexture);
+			}
 		}
-		GUI.EndGroup();
+				
+		icons = GameObject.FindGameObjectsWithTag ("PlayerUnit");
+		GUI.color = Color.blue;
+		foreach (var icon in icons) {
+			var mapPos = getMapPos (mapRect, icon.transform.position);
+			GUI.DrawTexture (new Rect (mapPos.x, mapPos.y, iconSize, iconSize),
+				iconTexture);
+		}
+		GUI.EndGroup ();
 					
-		if ( selectedSpawner != null ){
-			var spawner = selectedSpawner.GetComponent<Spawner>();
-			if(spawner!=null && spawner.currentUpgrade!=null){
+		if (selectedSpawner != null) {
+			var spawner = selectedSpawner.GetComponent<Spawner> ();
+			if (spawner != null && spawner.currentUpgrade != null) {
 				//display upgrade path selection
-				foreach(var upgrade in spawner.currentUpgrade.upgrades){
-					if( GUI.Button(upgrade.upgradeButtonRect, upgrade.upgradeButtonContent, upgrade.upgradeButtonStyle) ) {
-						gameObject.GetComponent<PlayerScript>().Upgrade(spawner, upgrade);
+				foreach (var upgrade in spawner.currentUpgrade.upgrades) {
+					if (GUI.Button (upgrade.upgradeButtonRect, upgrade.upgradeButtonContent, upgrade.upgradeButtonStyle)) {
+						gameObject.GetComponent<PlayerScript> ().Upgrade (spawner, upgrade);
 					}
 				}
 			}
 		}
 		
-		int resourceCount = gameObject.GetComponent<PlayerScript>().currentMoney;
-		GUI.TextArea( resourceTextRect, resourceCount.ToString(), resourceTextStyle );
+		int resourceCount = gameObject.GetComponent<PlayerScript> ().currentMoney;
+		GUI.TextArea (resourceTextRect, resourceCount.ToString (), resourceTextStyle);
 	}
 	
-	void UpdateSelection()
+	void UpdateSelection ()
 	{
-		if(selectedSpawner != null){
-			var spawner = selectedSpawner.GetComponent<Spawner>();
+		if (selectedSpawner != null) {
+			var spawner = selectedSpawner.GetComponent<Spawner> ();
 			//display upgrade path selection
-			foreach(var upgrade in spawner.currentUpgrade.upgrades){
-				if( Utilities.MouseInRectGUI( upgrade.upgradeButtonRect ) ) 
+			foreach (var upgrade in spawner.currentUpgrade.upgrades) {
+				if (Utilities.MouseInRectGUI (upgrade.upgradeButtonRect)) 
 					return;
 			}
 		}
 		
-		if ( Input.GetButtonDown ("Fire1") )
-		{
-			var ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+		if (Input.GetButtonDown ("Fire1")) {
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit rayHit;
-			if ( Physics.Raycast( ray, out rayHit, Mathf.Infinity, 1 << 9 ) )
-			{
-				if ( rayHit.transform.gameObject.GetComponent<Spawner>() != null )
+			if (Physics.Raycast (ray, out rayHit, Mathf.Infinity, 1 << 9)) {
+				if (rayHit.transform.gameObject.GetComponent<Spawner> () != null)
 					selectedSpawner = rayHit.transform.gameObject;
-			}
-			else
+			} else
 				selectedSpawner = null;
 		}
 		
-		if ( selectedSpawner != null )
-		{
+		if (selectedSpawner != null) {
 			selectionRing.transform.position = selectedSpawner.transform.position;	
-		}
-				
-		else
-		{
+		} else {
 			selectionRing.transform.position = Vector3.zero;
 		}
 	}
--- HeartGame/Assets/Scripts/UnitMovement.cs
+++ HeartGame/Assets/Scripts/UnitMovement.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class UnitMovement : MonoBehaviour {
+public class UnitMovement : MonoBehaviour
+{
 	
 	public Path path;
 	public int nodeIndex = 0;
@@ -12,172 +13,153 @@ public class UnitMovement : MonoBehaviour {
 	public float attackRadius = 10.0f;
 	public float rotateSpeed = Mathf.PI;
 	public float attackInterval = 1.0f;
-	
 	private GameObject targetEnemy = null;
 	private float nextAttackTime = 0.0f;
-	
 	public GameObject attackParticleEffect;
 	public GameObject deathParticleEffect;
 	public int health = 30;
 	public int damage = 10;
 	public float burstRadius = 0;
-	
 	private float killTime = Mathf.Infinity;
 	private int takeDamage = 0;
-	
 	private float lastPerceiveTime = -100.0f;
 	private GameObject enemyBase;
-	
 	private float currentSpeed;
-	
 	public bool attacking;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		this.gameObject.transform.position = path.nodes[nodeIndex].position;
+		this.gameObject.transform.position = path.nodes [nodeIndex].position;
 		isPlayerUnit = this.tag == "PlayerUnit";
 		
-		if (!isPlayerUnit)
-		{
-			HideGameObject(true, gameObject);	
+		if (!isPlayerUnit) {
+			HideGameObject (true, gameObject);	
 		}
 		
-		if ( isPlayerUnit )
-		{
-			enemyBase = GameObject.FindGameObjectWithTag("EnemyBase");
-		}
-		else
-		{
-			enemyBase = GameObject.FindGameObjectWithTag("PlayerBase");	
+		if (isPlayerUnit) {
+			enemyBase = GameObject.FindGameObjectWithTag ("EnemyBase");
+		} else {
+			enemyBase = GameObject.FindGameObjectWithTag ("PlayerBase");	
 		}
 		
 		attacking = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 		attacking = false;
 		
-		if ( killTime < Time.time )
-		{
-			GameObject.Destroy( gameObject );	
+		if (killTime < Time.time) {
+			GameObject.Destroy (gameObject);	
 		}
 		
-		if ( health <= 0 )
+		if (health <= 0)
 			return;
 		
-		if ( targetEnemy == null )
-		{
-			var enemies = Utilities.FindObjectsWithinRange(this.transform.position, isPlayerUnit ? "EnemyUnit" : "PlayerUnit", perceptRadius);
+		if (targetEnemy == null) {
+			var enemies = Utilities.FindObjectsWithinRange (this.transform.position, isPlayerUnit ? "EnemyUnit" : "PlayerUnit", perceptRadius);
 			
-			if ( enemies != null && enemies.Length > 0 )
-			{
+			if (enemies != null && enemies.Length > 0) {
 				float closestDistance = Mathf.Infinity;
-				foreach( var enemy in enemies )
-				{
-					enemy.GetComponent<UnitMovement>().lastPerceiveTime = Time.time;
-					if ( Vector3.Distance(enemy.transform.position, this.transform.position) < closestDistance )
-					{
-						closestDistance = Vector3.Distance(enemy.transform.position, this.transform.position);
+				foreach (var enemy in enemies) {
+					enemy.GetComponent<UnitMovement> ().lastPerceiveTime = Time.time;
+					if (Vector3.Distance (enemy.transform.position, this.transform.position) < closestDistance) {
+						closestDistance = Vector3.Distance (enemy.transform.position, this.transform.position);
 						targetEnemy = enemy;
 					}
 				}
 			}
 		}
 		
-		if ( targetEnemy != null )
-		{
-			if ( InRangeToAttack(targetEnemy) )
-			{
+		if (targetEnemy != null) {
+			if (InRangeToAttack (targetEnemy)) {
 				// attack enemy
-				Attack( targetEnemy );	
-			}
-			else
-			{
+				Attack (targetEnemy);	
+			} else {
 				// not in range to attack, move towards target enemy
-				MoveTo ( targetEnemy );
+				MoveTo (targetEnemy);
 			}			
 			
-			gameObject.transform.LookAt( targetEnemy.transform );
-		}
-		else
-		{
+			gameObject.transform.LookAt (targetEnemy.transform);
+		} else {
 			// are we near the base?
-			if ( Vector3.Distance( this.transform.position, enemyBase.transform.position) < perceptRadius )
-			{
+			if (Vector3.Distance (this.transform.position, enemyBase.transform.position) < perceptRadius) {
 				// move towards the enemy base
-				MoveTo ( enemyBase );
-				gameObject.transform.LookAt( enemyBase.transform.position );
+				MoveTo (enemyBase);
+				gameObject.transform.LookAt (enemyBase.transform.position);
 				
-				if ( InRangeToAttack( enemyBase ) )
-				{
+				if (InRangeToAttack (enemyBase)) {
 					// attack the enemy base
-					Attack( enemyBase );
+					Attack (enemyBase);
 				}
-			}
-			else
-			{			
+			} else {			
 				// move towards next path node
-				MoveTo ( path.nodes[nodeIndex] );
-				gameObject.transform.LookAt( path.nodes[nodeIndex].transform );
+				MoveTo (path.nodes [nodeIndex]);
+				gameObject.transform.LookAt (path.nodes [nodeIndex].transform);
 			}
 		}
 		
 		// reach node, target next one
-		if ( Vector3.Distance(this.gameObject.transform.position, path.nodes[nodeIndex].transform.position) < nodeRadius )
-		{
-			if ( nodeIndex < path.nodes.Count - 1 )
+		if (Vector3.Distance (this.gameObject.transform.position, path.nodes [nodeIndex].transform.position) < nodeRadius) {
+			if (nodeIndex < path.nodes.Count - 1)
 				nodeIndex++;
 		}
 		
-		TakeDamage();
+		TakeDamage ();
 		
-		if ( !isPlayerUnit )
-		{
-			HideGameObject( Time.time - lastPerceiveTime < 3.0f, gameObject );
+		if (!isPlayerUnit) {
+			HideGameObject (Time.time - lastPerceiveTime < 3.0f, gameObject);
 		}
 	}
 	
-	void HideGameObject(bool hide, GameObject hideMe)
+	public bool IsVisible ()
 	{
-		//hideMe.renderer.enabled = hide;
-		foreach(Transform child in hideMe.transform)
-		{
-			HideGameObject(hide, child.gameObject);	
+		if(gameObject.renderer == null)
+			return false;
+		return gameObject.renderer.enabled;
+	}
+	
+	void HideGameObject (bool hide, GameObject hideMe)
+	{
+		if (hideMe.renderer != null)
+			hideMe.renderer.enabled = hide;
+		
+		foreach (Transform child in hideMe.transform) {
+			HideGameObject (hide, child.gameObject);	
 		}
 	}
 	
-	void MoveTo(Transform other)
+	void MoveTo (Transform other)
 	{
 		MoveTo (other.position);
 	}
 	
-	void MoveTo(GameObject other)
+	void MoveTo (GameObject other)
 	{
 		MoveTo (other.transform);	
 	}
 	
-	void MoveTo(Vector3 other)
+	void MoveTo (Vector3 other)
 	{
-		this.gameObject.transform.position = Vector3.MoveTowards( this.gameObject.transform.position, other, moveSpeed );
+		this.gameObject.transform.position = Vector3.MoveTowards (this.gameObject.transform.position, other, moveSpeed);
 	}
 	
 	// checks if we are in range and facing the enemy
-	bool InRangeToAttack( GameObject enemy )
+	bool InRangeToAttack (GameObject enemy)
 	{
-		float distance = Vector3.Distance( transform.position, enemy.transform.position );
-		float angle = Vector3.Angle( transform.forward, enemy.transform.position - transform.position );
+		float distance = Vector3.Distance (transform.position, enemy.transform.position);
+		float angle = Vector3.Angle (transform.forward, enemy.transform.position - transform.position);
 		
-		if( angle < 30.0f )
-		{
-			RadiusScript enemyRadiusScript = enemy.GetComponent<RadiusScript>();
-			RadiusScript ourRadiusScript = gameObject.GetComponent<RadiusScript>();
+		if (angle < 30.0f) {
+			RadiusScript enemyRadiusScript = enemy.GetComponent<RadiusScript> ();
+			RadiusScript ourRadiusScript = gameObject.GetComponent<RadiusScript> ();
 			
-			if ( enemyRadiusScript != null )
+			if (enemyRadiusScript != null)
 				distance -= enemyRadiusScript.radius;
-			if ( ourRadiusScript != null )
+			if (ourRadiusScript != null)
 				distance -= ourRadiusScript.radius;
 			
 			distance -= attackRadius;
@@ -188,57 +170,53 @@ public class UnitMovement : MonoBehaviour {
 		return false;
 	}
 	
-	void Attack( GameObject enemy )
+	void Attack (GameObject enemy)
 	{
 		attacking = true;
 		
-		if ( nextAttackTime < Time.time )
-		{
+		if (nextAttackTime < Time.time) {
 			// attack
-			UnitMovement unitMovement = enemy.GetComponent<UnitMovement>();
-			PlayerScript playerScript = enemy.GetComponent<PlayerScript>();
+			UnitMovement unitMovement = enemy.GetComponent<UnitMovement> ();
+			PlayerScript playerScript = enemy.GetComponent<PlayerScript> ();
 			
-			if ( unitMovement != null ) {
+			if (unitMovement != null) {
 				//Handle AE burst
-				if(burstRadius <= 0)
-					unitMovement.Attacked( damage );
+				if (burstRadius <= 0)
+					unitMovement.Attacked (damage);
 				else {		
-					var enemies = Utilities.FindObjectsWithinRange(this.transform.position, isPlayerUnit ? "EnemyUnit" : "PlayerUnit", burstRadius);					
-					foreach( var other in enemies )
-					{
-						var otherMove = other.GetComponent<UnitMovement>();
-						if(otherMove !=null){
-							otherMove.Attacked(damage);
+					var enemies = Utilities.FindObjectsWithinRange (this.transform.position, isPlayerUnit ? "EnemyUnit" : "PlayerUnit", burstRadius);					
+					foreach (var other in enemies) {
+						var otherMove = other.GetComponent<UnitMovement> ();
+						if (otherMove != null) {
+							otherMove.Attacked (damage);
 						}
 					}
 				}
 			}
-			if ( playerScript != null )
-				playerScript.Attacked( damage );			
+			if (playerScript != null)
+				playerScript.Attacked (damage);			
 			
-			GameObject effect = (GameObject)Instantiate( attackParticleEffect, this.transform.position, this.transform.rotation );
+			GameObject effect = (GameObject)Instantiate (attackParticleEffect, this.transform.position, this.transform.rotation);
 			effect.transform.parent = gameObject.transform;
-			effect.transform.localPosition = new Vector3(0.0f, 0.0f, 1.0f);
-			GameObject.Destroy( effect, 0.5f );
+			effect.transform.localPosition = new Vector3 (0.0f, 0.0f, 1.0f);
+			GameObject.Destroy (effect, 0.5f);
 			nextAttackTime = Time.time + attackInterval;
 		}
 	}
 	
-	public void Attacked( int damage )
+	public void Attacked (int damage)
 	{
 		takeDamage += damage;
 	}
 	
-	void TakeDamage()
+	void TakeDamage ()
 	{
-		if ( health > 0 && takeDamage > 0 )
-		{
+		if (health > 0 && takeDamage > 0) {
 			health -= takeDamage;
 			
-			if ( health <= 0 )
-			{
-				GameObject effect = (GameObject)Instantiate( deathParticleEffect, this.transform.position, this.transform.rotation );
-				GameObject.Destroy( effect, 1.0f );
+			if (health <= 0) {
+				GameObject effect = (GameObject)Instantiate (deathParticleEffect, this.transform.position, this.transform.rotation);
+				GameObject.Destroy (effect, 1.0f);
 				killTime = Time.time + 1.0f;
 			}
 		}
